1d773a027075c9718085f8400a6f77abb5d89a41
Leivo Sepp <leivo@robootika.com>
2017-02-10 16:31:52 +0200

    Simpliing code

--- LessonLightSensorTSL2591/StartupTask.cs
+++ LessonLightSensorTSL2591/StartupTask.cs
@@ -7,8 +7,6 @@ namespace LessonLightSensorTSL2591
     {
         // TSL2591 Sensor
         private TSL2591 TSL2591Sensor = new TSL2591();
-        //byte gain = TSL2591.GAIN_MED;
-        //byte itime = TSL2591.INT_TIME_200MS;
 
         public void Run(IBackgroundTaskInstance taskInstance)
         {
